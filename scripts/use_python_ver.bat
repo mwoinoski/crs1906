65a31eb9d6a5c990e47f884e9c87fbbaf998fefa
Mike Woinoski <michaelw@articulatedesign.us.com>
2015-08-04 15:15:38 -0400

    Fixed bug in use_python_ver.bat

--- scripts/use_python_ver.bat
+++ scripts/use_python_ver.bat
@@ -1,7 +1,7 @@
 @echo off
 set tempfile=C:\cygwin\tmp\path
 
-python -c "import os, re; print(re.sub(r'python-.*?\\', r'python-"%1"\\', os.environ['PATH']))" > %tempfile%
+python -c "import os, re; print(re.sub(r'python-.*?([;\\])', r'python-"%1"\1', os.environ['PATH']))" > %tempfile%
 
 set /p PATH= < %tempfile%
 
