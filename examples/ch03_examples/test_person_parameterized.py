d4a77baf0f201e093aa79923b8bc9ab0af9ae9db
Mike Woinoski <mike.woinoski@gmail.com>
2020-02-26 12:07:17 -0500

    Add parameterized tests

---
+++ examples/ch03_examples/test_person_parameterized.py
@@ -0,0 +1,24 @@
+r"""
+Parameterized unit tests for Person
+
+Run as follows:
+cd C:\crs1906\examples\ch03_examples
+pytest test_person_parameterized.py
+"""
+
+from pytest import mark
+
+from person import Person
+
+
+@mark.parametrize('first, middle, last, expected', [
+    ("John", "", "Adams", "John Adams"), 
+    ("John", None, "Adams", "John Adams"), 
+    (None, None, "Coltrane", "Coltrane"), 
+    ("Miles", None, None, "Miles"), 
+    (None, "Quincy", None, "Quincy"), 
+])
+def test_full_name(first, middle, last, expected):
+    person = Person(first, middle, last)
+    full_name = person.full_name()
+    assert expected == full_name
