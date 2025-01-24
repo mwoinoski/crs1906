a371fa0f089bf8786d56737a74757d560843eeba
Mike Woinoski <michaelw@articulatedesign.us.com>
2025-01-24 18:11:53 -0500

    Updated all test_event_service_integration.py

--- exercises/solution_ex08_multiprocessing/tests/rest_services/test_event_service_integration.py
+++ exercises/solution_ex08_multiprocessing/tests/rest_services/test_event_service_integration.py
@@ -18,8 +18,7 @@ from tests.test_support.db_utils import (
 
 # SQLAlchemy can't connect to an in-memory SQLite database, so we'll
 # use a temporary database file.
-
-db_filename = 'test_db.sqlite'
+db_filename = r'C:\crs1906\tmp\test_db.sqlite'
 
 
 class EventServiceRestIntegrationTest(unittest.TestCase):
@@ -28,15 +27,16 @@ class EventServiceRestIntegrationTest(unittest.TestCase):
     """
 
     def setUp(self):
+        try:
+            drop_db_tables(db_filename)
+        except:
+            pass
         create_db_tables(db_filename)
         self.populate_db_tables()
 
-        settings = {'sqlalchemy.url': 'sqlite:///' + db_filename}
+        settings = {'sqlalchemy.url': f'sqlite:///{db_filename}'}
         app = main(None, **settings)
         self.testapp = TestApp(app)
-
-    def tearDown(self):
-        drop_db_tables(db_filename)
 
     def test_get_events_for_act_found(self):
         url = 'http://localhost:6543/rest/events/music.json?' \
@@ -52,30 +52,30 @@ class EventServiceRestIntegrationTest(unittest.TestCase):
             'year': 0,
             'events': [
                 {
-                    'id': 204,
-                    'venue': {
-                        'latitude': 40.7127,
-                        'prov_state': 'NY',
-                        'longitude': -74.0059,
-                        'name': 'Carnegie Hall',
-                        'country': 'USA',
-                        'id': 102,
-                        'street_address': '881 7th Ave, New York, NY 10019',
-                        'city': 'New York'
-                    },
-                    # 'price': '$180'  # FIXME: uncomment when database schema is complete
-                    # 'image_thumbnail': '/static/images/concerts-1.png',
-                    # 'image_banner': '/static/images/concerts.jpg',
-                },
-                {
                     'id': 201,
                     'venue': {
+                        'id': 101,
                         'latitude': 41.8369,
                         'prov_state': 'IL',
                         'longitude': -87.6847,
                         'name': 'Auditorium Theatre',
                         'country': 'USA',
+                        'street_address': 'E Congress Pkwy, Chicago, IL 60605',
+                        'city': 'Chicago'
+                    },
+                    # 'price': '$175'
+                    # 'image_thumbnail': '/static/images/concerts-4.png',
+                    # 'image_banner': '/static/images/concerts.jpg',
+                },
+                {
+                    'id': 202,
+                    'venue': {
                         'id': 101,
+                        'latitude': 41.8369,
+                        'prov_state': 'IL',
+                        'longitude': -87.6847,
+                        'name': 'Auditorium Theatre',
+                        'country': 'USA',
                         'street_address': 'E Congress Pkwy, Chicago, IL 60605',
                         'city': 'Chicago'
                     },
@@ -86,12 +86,12 @@ class EventServiceRestIntegrationTest(unittest.TestCase):
                 {
                     'id': 203,
                     'venue': {
+                        'id': 102,
                         'latitude': 40.7127,
                         'prov_state': 'NY',
                         'longitude': -74.0059,
                         'name': 'Carnegie Hall',
                         'country': 'USA',
-                        'id': 102,
                         'street_address': '881 7th Ave, New York, NY 10019',
                         'city': 'New York'
                     },
@@ -100,19 +100,19 @@ class EventServiceRestIntegrationTest(unittest.TestCase):
                     # 'image_banner': '/static/images/concerts.jpg',
                 },
                 {
-                    'id': 202,
+                    'id': 204,
                     'venue': {
-                        'latitude': 41.8369,
-                        'prov_state': 'IL',
-                        'longitude': -87.6847,
-                        'name': 'Auditorium Theatre',
+                        'id': 102,
+                        'latitude': 40.7127,
+                        'prov_state': 'NY',
+                        'longitude': -74.0059,
+                        'name': 'Carnegie Hall',
                         'country': 'USA',
-                        'id': 101,
-                        'street_address': 'E Congress Pkwy, Chicago, IL 60605',
-                        'city': 'Chicago'
+                        'street_address': '881 7th Ave, New York, NY 10019',
+                        'city': 'New York'
                     },
-                    # 'price': '$175'
-                    # 'image_thumbnail': '/static/images/concerts-4.png',
+                    # 'price': '$180'  # NEXT REV: uncomment when database schema is complete
+                    # 'image_thumbnail': '/static/images/concerts-1.png',
                     # 'image_banner': '/static/images/concerts.jpg',
                 },
             ]
@@ -121,11 +121,12 @@ class EventServiceRestIntegrationTest(unittest.TestCase):
         res = self.testapp.get(url, headers=headers, status=200)
 
         res_list = json.loads(str(res.body, 'utf-8'))
-        for e in res_list['events']:  # FIXME: delete when database schema is complete
+        for e in res_list['events']:  # NEXT REV: delete when database schema is complete
             del e['price']
             del e['image_thumbnail']
             del e['image_banner']
             del e['date_time']
+        res_list['events'] = sorted(res_list['events'], key=lambda event: event['id'])
         self.maxDiff = None
         self.assertEqual(expected_res, res_list)
 
@@ -139,29 +140,25 @@ class EventServiceRestIntegrationTest(unittest.TestCase):
         res_dict = json.loads(str(res.body, 'utf-8'))
         self.assertFalse(res_dict)
 
-    # def test_search_for_events_by_venue(self):
-    #     url = 'http://localhost:6543/rest/events/music/search?venue=Carnegie+Hall'
-    #     headers = {'Accept': 'application/json; charset=utf8'}
-    #     expected_res = [
-    #         {'act_id': 301, 'act_title': 'Berlin Philharmonic', 'act_type': 'music',
-    #          'event_id': 204, 'date_time': '2015-12-25 10:00:00',
-    #          'venue_name': 'Carnegie Hall', 'venue_city': 'New York',
-    #          'venue_prov_st': 'NY', 'venue_country': 'USA'},
-    #         {'act_id': 301, 'act_title': 'Berlin Philharmonic', 'act_type': 'music',
-    #          'event_id': 203, 'date_time': '2015-12-31 21:00:00',
-    #          'venue_name': 'Carnegie Hall', 'venue_city': 'New York',
-    #          'venue_prov_st': 'NY', 'venue_country': 'USA'},
-    #         {'act_id': 305, 'act_title': 'Take 6', 'act_type': 'music',
-    #          'event_id': 205, 'date_time': '2016-01-01 10:00:00',
-    #          'venue_name': 'Carnegie Hall', 'venue_city': 'New York',
-    #          'venue_prov_st': 'NY', 'venue_country': 'USA'},
-    #     ]
-    #
-    #     res = self.testapp.get(url, headers=headers, status=200)
-    #
-    #     res_list = json.loads(str(res.body, 'utf-8'))
-    #     self.maxDiff = None
-    #     self.assertEqual(expected_res, res_list)
+    def test_search_for_events_by_venue(self):
+        url = 'http://localhost:6543/rest/events/music.json?event_type=Venue&name=Carnegie+Hall&page=1&page_size=6'
+        headers = {'Accept': 'application/json; charset=utf8'}
+        expected_res = {
+            'id': 301,
+            'title': 'Berlin Philharmonic',
+            'notes': 'Beethoven and Brahms',
+            'act_type': 'music',
+            'year': 0,
+            'events': [],
+            'page': 1,
+            'page_size': 6,
+        }
+
+        res = self.testapp.get(url, headers=headers, status=200)
+
+        res_list = json.loads(str(res.body, 'utf-8'))
+        self.maxDiff = None
+        self.assertEqual(expected_res, res_list)
 
     def populate_db_tables(self):
         execute_insert(db_filename, 'acts',
