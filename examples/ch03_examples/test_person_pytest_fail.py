ccdfa9e615c02cab484f11c6c3157f27bc3510aa
Mike Woinoski <michaelw@articulatedesign.us.com>
2025-05-21 12:03:34 -0400

    Fixed Chap 3 example test_person_pytest_fail.py

--- examples/ch03_examples/test_person_pytest_fail.py
+++ examples/ch03_examples/test_person_pytest_fail.py
@@ -1,34 +1,41 @@
 r"""
-Unit tests for Person
+Unit tests for a buggy Person implementation.
 
 Run as follows:
 	cd C:\crs1906\examples\ch03_examples
 	pytest test_person_pytest_fail.py
 """
 
-from pytest import raises
-
 from person_buggy import Person
 
 __author__ = 'Mike Woinoski (michaelw@articulatedesign.us.com)'
 
 
-def test_init():
-	person = Person("John", "Quincy", "Adams")
-
-	assert ("John", "Quincy", "Adams") == \
-		   (person.first_name, person.middle_name, person.last_name)
-
 def test_full_name():
 	person = Person("John", "Quincy", "Adams")
-
 	full_name = person.full_name()
-
 	assert "John Quincy Adams" == full_name
 
-def test_eq_instances_equal():
-	p1 = Person("John", "Quincy", "Adams")
-	p2 = Person("John", "Quincy", "Adams")
 
-	assert p1 == p2  # == calls p1.__eq__(p2)
+def test_full_name_empty_middle():
+	person = Person("John", "", "Adams")
+	full_name = person.full_name()
+	assert "John Adams" == full_name
 
+
+def test_full_name_first_only():
+	person = Person("Miles", None, None)
+	full_name = person.full_name()
+	assert "Miles" == full_name
+
+
+def test_full_name_middle_only():
+	person = Person(None, "Quincy", None)
+	full_name = person.full_name()
+	assert "Quincy" == full_name
+
+
+def test_full_name_middle_and_last_only():
+	person = Person("", "Quincy", "Adams")
+	full_name = person.full_name()
+	assert "Quincy Adams" == full_name
