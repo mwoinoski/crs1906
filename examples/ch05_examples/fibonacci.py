43dad1dcd6f1724b9429f09cf3fbc573aaed5524
Mike Woinoski <michaelw@articulatedesign.us.com>
2025-05-23 16:33:49 -0400

    Tweaked fibonacci.py

--- examples/ch05_examples/fibonacci.py
+++ examples/ch05_examples/fibonacci.py
@@ -1,6 +1,18 @@
 """
-fibonacci.py -
+fibonacci.py - Fibonacci sequence generator
 """
+
+from argparse import ArgumentParser
+
+usage_msg = 'python fibonacci.py [-h|-v]'
+verbose = False
+
+def get_command_line_args():
+    parser = ArgumentParser(description='Fibonacci sequence generator',
+                            usage=usage_msg)
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='enable verbose output')
+    return parser.parse_args()
 
 
 def fib(n):
@@ -10,26 +22,30 @@ def fib(n):
     elif n == 1:
         return 1
     else:
-        return fib(n-1) + fib(n-2)
+        return fib(n - 1) + fib(n - 2)
 
 
 def fib_seq(n):
     seq = []
     if n > 0:
-        seq.extend(fib_seq(n-1))
+        seq.extend(fib_seq(n - 1))
     seq.append(fib(n))
     return seq
 
 
 def main():
-    for i in range(1, 20):
+    values = 20
+    for i in range(1, values + 1):  # noqa
         next_fib_seq = fib_seq(i)
-        print(f'First {i+1} Fibonacci numbers: {next_fib_seq}')
+    if verbose and i == values:  # noqa
+        print(f'First {values} Fibonacci numbers: {next_fib_seq}')  # noqa
 
 
 if __name__ == '__main__':
-    print("Starting fibonacci...")
+    verbose = get_command_line_args().verbose
+
     main()
+
     # from timeit import timeit
     # loops = 100
     # total_time = timeit("main()",
@@ -37,5 +53,4 @@ if __name__ == '__main__':
     #                    number=loops)
     # avg_time_per_call = total_time / loops
     # print(f'Called main() {loops} times,',
-    #       f'average time per call was {avg_time_per_call:.3f} seconds')
-    print('Done.')
+    #       f'average time per call was {avg_time_per_call:.3f} seconds')  # noqa
