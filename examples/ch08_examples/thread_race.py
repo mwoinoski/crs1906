67dab459ad581f0e604edd4e48c27e0b50f28766
Mike Woinoski <mike.woinoski@gmail.com>
2026-03-20 13:38:26 -0400

    Modified ch08_examples/thread_race.py so there's only 1 producer

--- examples/ch08_examples/thread_race.py
+++ examples/ch08_examples/thread_race.py
@@ -2,62 +2,75 @@
 Demo of a race condition.
 
 Depending on the performance of your VM, you may need either to increase or
-decrease the value of the global `goal` variable to get interesting results.
+decrease the values of the global RACE_CHANCE and PRODUCER_DELAY variables to
+get interesting results.
 """
 
 from threading import Thread
-import time, os
+import time, os, random
+
+# Because this code is so simple, the race condition will rarely occur unless
+# we hack the code a bit. The following values introduce processing delays
+# that increase the chance of the race condition. But in production code,
+# race conditions happen with no additional coaxing :)
+
+# Tweak the following values as needed to get an occasional race condition.
+# Make one or both values larger to increase the rate of failure.
+# Make them smaller to decrease the rate of failure
+RACE_CHANCE = 0.10  # widen the dangerous check-then-pop gap 10% of the time
+PRODUCER_DELAY = 0.0005  # slow down the producer
 
 buffer = []
 
-sentinel = object()  # value that means "end of data"
+sentinel = object()
+
 produced = 0
 consumed = 0
-
-goal = 500  # you may need to tweak this value
-expected = 2 * goal  # two producers should produce this many items
+expected = 100
+stop = False
 
 def producer(pid):
     global produced
-    for i in range(goal):
+    for i in range(expected):
         buffer.append((pid, i))
         produced += 1
-        time.sleep(0)   # hack to increase the chance of a race condition
-    buffer.append(sentinel)  # add sentinel to end of buffer
+        time.sleep(PRODUCER_DELAY)   # faster producer
 
+    buffer.append(sentinel)
 
 def consumer(cid):
-    global consumed, expected
-    while True:
-        if len(buffer) > 0:  # check that there is an item to pop
-            time.sleep(0)    # hack to increase the chance of a race condition
+    global consumed, stop
+    while not stop:
+        if len(buffer) > 0:
+            # widen the race window only sometimes
+            if random.random() < RACE_CHANCE:
+                time.sleep(0)
+
             try:
                 item = buffer.pop(0)
                 if item is sentinel:
+                    stop = True
                     return
-                else:
-                    consumed += 1
+                consumed += 1
             except IndexError:
                 print("pop failed because buffer was empty", flush=True)
                 print("race detected by consumer", cid, flush=True)
                 print_stats(expected, produced, consumed)
-                os._exit(1)  # hard exit
+                os._exit(1)
         else:
-            time.sleep(0) # yield the CPU so another thread has a chance
+            time.sleep(0)
 
-
-def print_stats(expected, produced, consumed):
-    print("expected:", expected)
-    print("produced:", produced)
-    print("consumed:", consumed)
-
+def print_stats(items_expected, items_produced, items_consumed):
+    print("expected:", items_expected)
+    print("produced:", items_produced)
+    print("consumed:", items_consumed)
 
 threads = [
     Thread(target=producer, args=(1,)),
-    Thread(target=producer, args=(2,)),
     Thread(target=consumer, args=(1,)),
     Thread(target=consumer, args=(2,))
 ]
+
 for t in threads:
     t.start()
 for t in threads:
